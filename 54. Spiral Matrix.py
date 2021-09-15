07b7b378fb27a646d651199532867811819e8744
RoyLizhiyu <44133321+RoyLizhiyu@users.noreply.github.com>
2021-09-15 15:46:41 -0400

    Update 54. Spiral Matrix.py

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,35 +1,43 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        up = 0
+        left = 0
+        right = len(matrix[0]) -1
+        bot = len(matrix) - 1
         result = []
-        rows, columns = len(matrix), len(matrix[0])
-        up = left = 0
-        right = columns - 1
-        down = rows - 1
 
-        while len(result) < rows * columns:
-            # Traverse from left to right.
-            for col in range(left, right + 1):
-                result.append(matrix[up][col])
+        while len(result) < len(matrix[0]) * len(matrix):
+            # traverse from left to right, row fixed, column changing.
+            for i in range(left, right+1):
+                result.append(matrix[up][i])
+            
+            # traverse from top to down. row changing, column fixed.
+            for i in range(up+1, bot+1):
+                result.append(matrix[i][right])
+                
+            #traverse from right to left. Make sure we only traverse
+            # the row we haven't been. That means we only go from right to left
+            # when up and bot haven't become the same row.
+        
+            if up != bot:
+                for i in range(right-1, left-1, -1):
+                    result.append(matrix[bot][i])
+            
+            #travere from bot to up. Make sure we only traverse the column
+            # we haven't been. That means we only go from bot to top when
+            # left and right haven't become the same column.
 
-            # Traverse downwards.
-            for row in range(up + 1, down + 1):
-                result.append(matrix[row][right])
-
-            # Make sure we are now on a different row.
-            if up != down:
-                # Traverse from right to left.
-                for col in range(right - 1, left - 1, -1):
-                    result.append(matrix[down][col])
-
-            # Make sure we are now on a different column.
             if left != right:
-                # Traverse upwards.
-                for row in range(down - 1, up, -1):
-                    result.append(matrix[row][left])
-
-            left += 1
-            right -= 1
+                for i in range(bot-1, up, -1):
+                    result.append(matrix[i][left])
+                    
+            #update the pointers,, make sure they squeeze the rectangle
             up += 1
-            down -= 1
-
+            left  += 1
+            right -= 1
+            bot -= 1
         return result
